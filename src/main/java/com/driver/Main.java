84940353beb21920dcfb1fe6928066af760bf9d4
y-paradox <yadavakshat586@gmail.com>
2023-02-19 00:00:10 +0530

    Encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        RWOnly rwo = new RWOnly();
+
+        rwo.setName("Akshat");
+
+        System.out.println(rwo.getName());
+    }
   
 }
